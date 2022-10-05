00f7edcab87e3918901f73ce924c087336cb19ab
YD-0110 <90946473+YD-0110@users.noreply.github.com>
2022-10-05 11:39:49 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw(){
+    
+}
+
+function take_snapshot(){
+    save('look at my photo.png');
+}
+
